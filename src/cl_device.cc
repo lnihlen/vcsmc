4bf493d5dab2fb34df19f6c84997b0c9e34e42c2
Luke Nihlen <luke.nihlen@gmail.com>
2014-05-13 21:01:55 -0700

    checkpointing before deprecating cl_device as it currently stands

--- src/cl_device.cc
+++ src/cl_device.cc
@@ -1,14 +1,33 @@
 #include "cl_device.h"
 
-#include <iostream>
 #include <condition_variable>
 #include <mutex>
 #include <thread>
+#include <unordered_map>
 #include <OpenCL/OpenCL.h>
 
+#include "cl_kernel.h"
 #include "cl_task.h"
 
 namespace vcsmc {
+
+static CLDevice::CLDevice* instance_ = NULL;
+
+// Technically both a kernel and a program, we assume a 1:1 relationship between
+// .cl files in the "kernels/" directory so lump them together, and choose the
+// name "program" because "kernel" aleady has meaning within vcsmc.
+struct CLProgram {
+  cl_program program;
+
+  ~CLProgram() {
+    clReleaseProgram(program);
+  };
+};
+
+struct CLEvent {
+  cl_event event;
+
+};
 
 struct CLDevice::Impl {
   cl_device_id device_id;
@@ -20,7 +39,6 @@ struct CLDevice::Impl {
   std::unique_lock<std::mutex> cv_lock;
   std::mutex cv_mutex;
 
-  // Protects tasks
   std::mutex tasks_mutex;
   std::list<std::unique_ptr<CLTask>> tasks;
 
@@ -28,15 +46,28 @@ struct CLDevice::Impl {
   // Not protected by lock.
   bool should_quit;
 
+  std::mutex state_mutex;
+  CLState state;
+
+  // Our cache of previously loaded and compiled programs.
+  typedef std::unordered_map<std::string, std::unique_ptr<CLProgram>>
+      ProgramMap;
+  ProgramMap program_map;
+
+  uint64 event_handles;
+  typedef std::unordered_map<uint64, std::unique_ptr<CLProgram>> CallbackMap;
+  CallbackMap callback_map;
+
   Impl()
       : cv_lock(cv_mutex),
-        should_quit(false) { }
+        should_quit(false),
+        state(NOT_STARTED),
+        event_handles(0)  { }
 };
 
-static CLDevice::CLDevice* instance_ = NULL;
 
 // static
-bool CLDevice::Setup() {
+bool CLDevice::Initialize() {
   instance_ = new CLDevice;
   instance_->impl_->thead = std::thread(&CLDevice::RunLoop, instance_);
 }
@@ -48,45 +79,96 @@ bool CLDevice::Teardown() {
 CLDevice::CLDevice() : impl_(new Impl) {
 }
 
-void CLDevice::RunLoop() {
+bool CLDevice::InitializeOnOwnThread() {
   int result = clGetDeviceIDs(
       NULL, CL_DEVICE_TYPE_GPU, 1, &impl_->device_id, NULL);
-  if (result != CL_SUCCESS) {
-    std::cerr << "failed to create device group: " << result << std::endl;
-    return;
-  }
+  if (result != CL_SUCCESS)
+    return false;
 
   impl_->context = clCreateContext(
       0, 1, &impl_->device_id, NULL, NULL, &result);
-  if (!context) {
-    std::cerr << "failed to create compute context: " << result <<  std::endl;
-    return;
-  }
+  if (!context)
+    return false;
 
   impl_->command_queue = clCreateCommandQueue(
       context, impl_->device_id, 0, &result);
-  if (!impl_->command_queue) {
-    std::cerr << "failed to create command queue: " << result << std::endl;
+  if (!impl_->command_queue)
+    return false;
+
+  return true;
+}
+
+void CLDevice::RunLoop() {
+  SetState(INITIALIZING);
+
+  if (!InitializeOnOnThread()) {
+    SetState(FATAL_ERROR);
     return;
   }
+
+  SetState(READY);
 
   // Main loop. Wait for task enqueue.
   while (!should_quit) {
     impl_->cv.wait();
 
-    std::unique_ptr<Task> current_task;
+    std::unique_ptr<Task> task;
 
     {
-      std::lock_guard<std::mutex> lock(tasks_mutex);
+      std::lock_guard<std::mutex> lock(impl_->tasks_mutex);
       if (tasks.size()) {
-        current_task = std::move(tasks.front());
+        task = std::move(tasks.front());
         tasks.pop_front();
       }
     }
 
-    if (!current_task)
+    if (!task)
       continue;
+
+    ProgramMap::iterator pg = impl_->program_map.find(task->program_name());
+    // Load and compile program if we have not yet done so.
+    if (pg == impl_->program_map.end()) {
+      if (!LoadProgram(task->program_name())) {
+        task->OnFailure();
+      }
+      pg = impl_->program_map.find(task->program_name());
+    }
+
+    assert(pg != impl_->program_map.end());
+
+    int result;
+
+    // Make a kernel and connect the arguments.
+    cl_kernel kernel = clCreateKernel(
+        program_map.find((*pg)->program, task->program_name(), &result));
+    if (result != CL_SUCCESS) {
+      SetState(FATAL_ERROR);
+      return;
+    }
+
+    size_t arg = 0
+    for (size_t i = 0; i < task->inputs(); ++i) {
+      result |= clSetKernelArg(
+          kernel, arg++, task->input(i).first, task->input(i).second);
+    }
+    for (size_t i = 0; i < task->outputs(); ++i) {
+      result |= clSetKernelArg(
+          kernel, arg++, task->output(i).first, task->output(i).second);
+    }
+    if (result != CL_SUCCESS) {
+      SetState(FATAL_ERROR);
+      return;
+    }
+
+    // Create an event and connect it to our callback function.
+
+    // Enqueue.
   }
+}
+
+void CLDevice::SetState(CLState state) {
+  std::lock_guard<std::mutex> lock(impl_->state_mutex);
+  impl_->state = state;
 }
 
 }  // namespace vcsmc
--- src/cl_device.h
+++ src/cl_device.h
@@ -1,27 +1,42 @@
 #ifndef SRC_CL_DEVICE_H_
 #define SRC_CL_DEVICE_H_
 
+#include <string>
+
 namespace vcsmc {
+
+class CLTask;
 
 // Simplisticly represents an OpenCL Device, Context, and Command Queue.
 class CLDevice {
  public:
+
   // Spawns the thread to actually setup OpenCL.
-  static void Setup();
-  static void Teardown();
+  static void Initialize();
+  static void Terminate();
 
-  class Task {
-   public:
-    Task();
-
-   private:
+  enum CLState {
+    NOT_STARTED,
+    INITIALIZING,
+    READY,
+    TERMINATING,
+    STOPPED,
+    FATAL_ERROR
   };
+
+  // Takes ownership of task. Guaranteed FIFO order
+  static void Enqueue(std::unique_ptr<CLTask> task);
 
  private:
   CLDevice();
+  bool InitializeOnOwnThread();
   void RunLoop();
+  void SetState(CLState state);
 
   static CLDevice* instance_;
+
+  bool LoadProgram(const std::string& name);
+
   // Keep the OpenCL state block in the .cc to avoid polluting the header.
   struct Impl;
   std::unique_ptr<Impl> impl_;
