fe8b4a3a9ccf99784c91ffa6e80613afb5d45a37
Luke Nihlen <luke.nihlen@gmail.com>
2015-01-25 17:04:56 -0800

    starts check.cc

---
+++ src/check.cc
@@ -0,0 +1,18 @@
+// check - compares a binary and sim image to a screenshot from Stella
+
+#include <cassert>
+#include <cstdio>
+#include <fcntl.h>
+#include <memory>
+#include <stdio.h>
+#include <string>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+#include "image.h"
+#include "image_file.h"
+
+int main(int argc, char* argv[]) {
+}
