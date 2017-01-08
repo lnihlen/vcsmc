df7ad93487cb1538ce0b95fe1c9298218d2f4c0d
Luke Nihlen <luke.nihlen@gmail.com>
2017-01-07 20:12:07 -0800

    just compare atari palette against all colors

--- src/find_ciede_max_distance.cc
+++ src/find_ciede_max_distance.cc
@@ -2,20 +2,17 @@
 // Ciede2K distance between any two RGB colors. Used for normalizing Ciede2K
 // distances to [0, 1] range.
 
-#include <chrono>
-#include <cinttypes>
 #include <stdio.h>
 
 #include "tbb/tbb.h"
 
 #include "color.h"
+#include "color_table.h"
+#include "constants.h"
 #include "types.h"
 
-static const uint32 kStartColorABGR = 0x00000000;
-static const uint32 kStopColorABGR = 0x00ffffff;
-static const uint64 kTotalEvals = static_cast<uint64>(kStopColorABGR) *
-    static_cast<uint64>(kStopColorABGR / 2);
-static const uint32 kStepSize = 255;
+const uint32 kStartColorABGR = 0x00000000;
+const uint32 kStopColorABGR = 0x00ffffff;
 
 struct MaxDistance {
  public:
@@ -38,10 +35,9 @@ class SearchABGRJob {
     for (uint32 i = r.begin(); i < r.end(); ++i) {
       double lab_a[4];
       vcsmc::RGBAToLabA(reinterpret_cast<const uint8*>(&i), lab_a);
-      for (uint32 j = i + 1; j <= kStopColorABGR; ++j) {
-        double lab_b[4];
-        vcsmc::RGBAToLabA(reinterpret_cast<const uint8*>(&j), lab_b);
-        double distance = vcsmc::Ciede2k(lab_a, lab_b);
+      for (uint32 j = 0; j < vcsmc::kNTSCColors; ++j) {
+        double distance = vcsmc::Ciede2k(lab_a,
+            vcsmc::kAtariNTSCLabColorTable + (4 * j));
         if (distance > local_max_distance.distance) {
           local_max_distance.color_a = i;
           local_max_distance.color_b = j;
@@ -60,43 +56,18 @@ int main(int argc, char* argv[]) {
   (void)argv;
 
   MaxDistances max_distances;
-  uint32 start_range = kStartColorABGR;
-  uint32 end_range = start_range + kStepSize;
-  uint64 total_evals = 0;
 
-  while (start_range < kStopColorABGR) {
-    auto start_of_run = std::chrono::high_resolution_clock::now();
-    tbb::parallel_for(
-        tbb::blocked_range<uint32>(start_range, end_range),
-        SearchABGRJob(max_distances));
-    auto end_of_run = std::chrono::high_resolution_clock::now();
-    uint64 time_us = std::chrono::duration_cast<std::chrono::microseconds>(
-        end_of_run - start_of_run).count();
+  tbb::parallel_for(
+      tbb::blocked_range<uint32>(kStartColorABGR, kStopColorABGR),
+      SearchABGRJob(max_distances));
+  MaxDistance md = max_distances.combine(
+      [](const MaxDistance& md_a, const MaxDistance& md_b) {
+        if (md_a.distance > md_b.distance) return md_a;
+        return md_b;
+      });
 
-    uint64 evals = 0;
-    for (uint32 i = start_range; i < end_range; ++i) {
-      evals += kStopColorABGR - i;
-    }
-    total_evals += evals;
-    uint64 rate = (evals * 100000) / time_us;
-    uint64 eta = (kTotalEvals - evals) / rate;
-    double completion = (static_cast<double>(total_evals) * 100.0) /
-        static_cast<double>(kTotalEvals);
-    printf("percent done: %3.7f, rate per second: %" PRIu64 
-        ", eta in seconds: %" PRIu64 " ",
-        completion, rate, eta);
-    MaxDistance md = max_distances.combine(
-        [](const MaxDistance& md_a, const MaxDistance& md_b) {
-          if (md_a.distance > md_b.distance) return md_a;
-          return md_b;
-        });
-
-    printf("max distance: %.19g, bgr_a: %06x, bgr_b: %06x\n",
-        md.distance, md.color_a, md.color_b);
-
-    start_range = end_range;
-    end_range = std::min(start_range + kStepSize, kStopColorABGR + 1);
-  }
+  printf("max distance: %.19g, bgr_a: %06x, bgr_b: %06x\n",
+      md.distance, md.color_a, md.color_b);
 
   return 0;
 }
