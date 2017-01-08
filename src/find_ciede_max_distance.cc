7d21911b1f9e1f1ac7580532804bcf2cab6dde64
Luke Nihlen <luke.nihlen@gmail.com>
2017-01-07 18:37:13 -0800

    adding eta timing to see if this is tractable

--- src/find_ciede_max_distance.cc
+++ src/find_ciede_max_distance.cc
@@ -2,6 +2,7 @@
 // Ciede2K distance between any two RGB colors. Used for normalizing Ciede2K
 // distances to [0, 1] range.
 
+#include <chrono>
 #include <stdio.h>
 
 #include "tbb/tbb.h"
@@ -11,6 +12,9 @@
 
 static const uint32 kStartColorABGR = 0x00000000;
 static const uint32 kStopColorABGR = 0x00ffffff;
+static const uint64 kTotalEvals = (kStopColorABGR - kStartColorABGR) *
+  ((kStopColorABGR - kStartColorABGR) / 2);
+static const uint32 kStepSize = 255;
 
 struct MaxDistance {
  public:
@@ -33,7 +37,7 @@ class SearchABGRJob {
     for (uint32 i = r.begin(); i < r.end(); ++i) {
       double lab_a[4];
       vcsmc::RGBAToLabA(reinterpret_cast<const uint8*>(&i), lab_a);
-      for (uint32 j = i; j <= kStopColorABGR; ++j) {
+      for (uint32 j = i + 1; j <= kStopColorABGR; ++j) {
         double lab_b[4];
         vcsmc::RGBAToLabA(reinterpret_cast<const uint8*>(&j), lab_b);
         double distance = vcsmc::Ciede2k(lab_a, lab_b);
@@ -55,9 +59,29 @@ int main(int argc, char* argv[]) {
   (void)argv;
 
   MaxDistances max_distances;
-  tbb::parallel_for(
-      tbb::blocked_range<uint32>(kStartColorABGR, kStopColorABGR + 1),
-      SearchABGRJob(max_distances));
+  uint32 start_range = kStartColorABGR;
+  uint32 end_range = start_range + kStepSize;
+  uint64 evals = 0;
+
+  auto start_of_run = std::chrono::high_resolution_clock::now();
+  while (start_range < kStopColorABGR) {
+    tbb::parallel_for(
+        tbb::blocked_range<uint32>(start_range, end_range),
+        SearchABGRJob(max_distances));
+    auto end_of_run = std::chrono::high_resolution_clock::now();
+    uint64 time_us = std::chrono::duration_cast<std::chrono::microseconds>(
+        end_of_run - start_of_run).count();
+    for (uint32 i = start_range; i < end_range; ++i) {
+      evals += kStopColorABGR - i;
+    }
+    uint64 rate = (evals * 100000) / time_us;
+    uint64 eta = (kTotalEvals - evals) / rate;
+
+    printf("rate per second: %llu, eta in seconds: %llu\n", rate, eta);
+
+    start_range = end_range;
+    end_range = std::min(start_range + kStepSize, kStopColorABGR + 1);
+  }
 
   MaxDistance md = max_distances.combine(
       [](const MaxDistance& md_a, const MaxDistance& md_b) {
