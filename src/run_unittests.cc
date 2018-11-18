9a5c1bd7e352434f993358f8c61d9c6cc4dd663e
Luke Nihlen <luke.nihlen@gmail.com>
2018-11-17 22:43:20 -0800

    more CUDA cleanup

--- src/run_unittests.cc
+++ src/run_unittests.cc
@@ -2,8 +2,6 @@ extern "C" {
 #include "libz26/libz26.h"
 }
 
-#include "cuda.h"
-#include "cuda_runtime.h"
 #include "gtest/gtest.h"
 
 #include "cuda_utils.h"
@@ -13,7 +11,6 @@ int main(int argc, char* argv[]) {
   ::testing::FLAGS_gtest_death_test_style = "threadsafe";
 
   init_z26_global_tables();
-  if (!vcsmc::InitializeCuda(false)) return -1;
 
   {
     int result = RUN_ALL_TESTS();
